f02fcad19d44bdc7fe32c7739efd285aa6190026
Praveen E <prav@MacBook-Pro-479.local>
2020-02-04 11:49:02 -0800

    Check if file is none before passing to st.image

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Wildfire Detection")
 file_obj = st.file_uploader('Choose an image:', ('jpg', 'jpeg'))
-st.image(file_obj)
+if file_obj is not None:
+    st.image(file_obj)
 st.write("No fire!")
 
